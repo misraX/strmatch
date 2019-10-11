39aee45a45230bd6063bf8d4c987f623172459aa
misraX <maysragamal@gmail.com>
2019-10-11 15:08:27 +0200

    Adding strmatch and test_strmatch

---
+++ strmatch.py
@@ -0,0 +1,29 @@
+class Finder(object):
+    def __init__(self, string_list):
+        """
+        Example:
+        >>> string_list = ['hello', 'world']
+        >>> finder_cls = Finder(string_list)
+
+        :param string_list: list of strings
+        """
+        self.string_list = string_list
+
+    def find(self, value):
+        """
+        Finds string in a list of strings.
+
+        The find method main functionality is evaluating the intersection of two sets.
+
+        The process:
+
+        1. Iterating over the string_list
+        2. Check if there is an intersection between the matcher and the value.
+        3. While True add it to the list.
+
+        Complexity of set intersection = O(min(len(s), len(t))
+
+        :param value: str
+        :return: list
+        """
+        return [matcher for matcher in self.string_list if set.intersection(set(matcher), set(value))]
---
+++ test_strmatch.py
@@ -0,0 +1,46 @@
+import unittest
+
+from strmatch import Finder
+
+
+class FinderTestCase(unittest.TestCase):
+    def setUp(self):
+        self.finder = Finder
+
+    def test_finder_cls(self):
+        """Initialize Finder with a string_list or raise exception"""
+        with self.assertRaises(TypeError) as context:
+            finder_cls = self.finder()
+        string_list = ['str', 'finder', 'example']
+        finder_cls = self.finder(string_list)
+        self.assertEqual(string_list, finder_cls.string_list)
+
+    def test_match_short_list_of_strings(self):
+        """Match short list of strings"""
+        string_list = ['asd', 'asdd', 'fre', 'glk', 'lkm']
+        finder_cls = self.finder(string_list)
+        find_str = finder_cls.find('sad')
+        expected = ['asd', 'asdd']
+        self.assertEqual(find_str, expected)
+
+    def test_match_long_list_of_strings(self):
+        """Match long list of strings"""
+        long_string = ['nfjte', 'wiggp', 'lqgipxuglnne', 'rdrsqztta', 'jpdcc', 'iytucm', 'jfrbbt', 'czvvtutj',
+                       'xwvorizq', 'ju', 'ktdlek',
+                       'yqouiypvjzdv', 'qwpmp', 'bwlzzvpmzz', 'sblqoazhx', 'doimcsggit', 'ssuqhibgecrbtvt', 'lvhtw',
+                       'fcltvta', 'uxchxr', 'yzf', 'ylebsz']
+        finder_cls = self.finder(long_string)
+        find_str = finder_cls.find('n')
+        excepted = ['nfjte', 'lqgipxuglnne']
+        self.assertEqual(find_str, excepted)
+
+        find_str = finder_cls.find('jfe')
+        excepted = ['nfjte', 'lqgipxuglnne', 'jpdcc', 'jfrbbt', 'czvvtutj', 'ju',
+                    'ktdlek', 'yqouiypvjzdv', 'ssuqhibgecrbtvt', 'fcltvta', 'yzf', 'ylebsz']
+        self.assertEqual(find_str, excepted)
+
+        find_str = finder_cls.find('jfeqzs')
+        excepted = ['nfjte', 'lqgipxuglnne', 'rdrsqztta', 'jpdcc', 'jfrbbt', 'czvvtutj', 'xwvorizq', 'ju', 'ktdlek',
+                    'yqouiypvjzdv', 'qwpmp', 'bwlzzvpmzz', 'sblqoazhx', 'doimcsggit', 'ssuqhibgecrbtvt', 'fcltvta',
+                    'yzf', 'ylebsz']
+        self.assertEqual(find_str, excepted)
